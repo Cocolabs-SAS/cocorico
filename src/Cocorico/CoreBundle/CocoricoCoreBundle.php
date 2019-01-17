e7c1f77d8be94ee3fe603e387cb461c9c27e592a
Tiretdusix <44053407+tiretdusix@users.noreply.github.com>
2019-01-17 16:24:28 +0100

    Start to Fix Twig Translation extension overriding

--- src/Cocorico/CoreBundle/CocoricoCoreBundle.php
+++ src/Cocorico/CoreBundle/CocoricoCoreBundle.php
@@ -11,6 +11,8 @@
 
 namespace Cocorico\CoreBundle;
 
+use Cocorico\CoreBundle\DependencyInjection\Compiler\TwigTranslationExtensionCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -19,4 +21,12 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class CocoricoCoreBundle extends Bundle
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new TwigTranslationExtensionCompilerPass());
+    }
 }
---
+++ src/Cocorico/CoreBundle/DependencyInjection/Compiler/TwigTranslationExtensionCompilerPass.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Cocorico package.
+ *
+ * (c) Cocolabs SAS <contact@cocolabs.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cocorico\CoreBundle\DependencyInjection\Compiler;
+
+use Cocorico\CoreBundle\Twig\TranslationExtension;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class TwigTranslationExtensionCompilerPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        /**
+         * Override Twig Translation extension
+         */
+        if ($container->hasDefinition('twig.extension.trans')) {
+            $definition = $container->getDefinition('twig.extension.trans');
+            $definition->setClass(TranslationExtension::class);
+            $definition->addMethodCall(
+                'setCheckTranslation',
+                array($container->getParameter('cocorico.check_translation'))
+            );
+        }
+    }
+}
