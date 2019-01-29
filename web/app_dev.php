6f0ea7bb3af9bd72530b9490541ac2ec4a409564
uzurstuv <maryan.nazar@cocolabs.io>
2019-01-29 13:04:24 +0200

    Change directory structure and autoload process

--- web/app_dev.php
+++ web/app_dev.php
@@ -4,15 +4,14 @@ use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
+// read https://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
 // for more information
 //umask(0000);
-
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
 ) {
 //    header('HTTP/1.0 403 Forbidden');
 //    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
@@ -21,11 +20,10 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 /**
  * @var Composer\Autoload\ClassLoader $loader
  */
-$loader = require __DIR__ . '/../app/autoload.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
--- web/app_staging.php
+++ web/app_staging.php
@@ -21,11 +21,10 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 /**
  * @var Composer\Autoload\ClassLoader $loader
  */
-$loader = require __DIR__ . '/../app/autoload.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('staging', true);
-$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
