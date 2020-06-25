41589359e16fcf6560530d9876546b325832963f
Pieterjan Montens <pieterjan@montens.net>
2020-06-25 23:53:11 +0200

    :wrench: updating parameters and deploy script to include mailer info
    ça marche !

--- clevercloud/pre_build_hook.sh
+++ clevercloud/pre_build_hook.sh
@@ -3,12 +3,22 @@ echo "=====> Executing prepare.sh clevercloud init script"
 cp ./clevercloud/init/parameters.yml ./app/config/parameters.yml
 
 # Custom substitute symfony env vars, because env preprocessor fails to work
-var_list="GGL_KEY1 GGL_KEY2 BASE_SCHEME BASE_HOST MYSQL_ADDON_HOST MYSQL_ADDON_PORT MYSQL_ADDON_DB MYSQL_ADDON_USER MYSQL_ADDON_PASSWORD MONGODB_ADDON_DB MONGODB_ADDON_URI"
+var_list="GGL_KEY1 GGL_KEY2 \
+SMTP_HOST SMTP_USER SMTP_PASSWORD SMTP_PORT \
+BASE_SCHEME BASE_HOST \
+MYSQL_ADDON_HOST MYSQL_ADDON_PORT MYSQL_ADDON_DB MYSQL_ADDON_USER MYSQL_ADDON_PASSWORD \
+MONGODB_ADDON_DB MONGODB_ADDON_URI"
+
 for key in $var_list; do
     echo "Setting $key in parameters.yml"
     sed -i -e 's|%'"$key"'%|'"${!key}"'|' ./app/config/parameters.yml
 done
+
+sleep 2
+echo "##### FINAL APP CONFIG #####"
 cat ./app/config/parameters.yml
+echo "############################"
+sleep 2
 
 # Install persistent upload folder
 echo "Synchronising upload folder"
