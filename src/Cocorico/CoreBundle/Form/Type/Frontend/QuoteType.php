3228a4b478ac0777b520ecd71c51c8ee916a5a22
pmontens <pieterjan@montens.net>
2020-07-07 15:32:58 +0200

    :hammer: debugging quote form

--- src/Cocorico/CoreBundle/Form/Type/Frontend/QuoteType.php
+++ src/Cocorico/CoreBundle/Form/Type/Frontend/QuoteType.php
@@ -29,16 +29,16 @@ class QuoteType extends AbstractType
         $quote = $builder->getData();
 
         $builder
-            ->add('budget_estimation', NumberType::class)
-            ->add('presta_start_date',
-                DateType::class,
-                array_merge(
-                    array(
-                        'property_path' => 'start',
-                        'widget' => 'single_text',
-                        'format' => 'dd/MM/yyyy',
-                    )
-                ))
+            //->add('budget_estimation', NumberType::class)
+            //->add('presta_start_date',
+            //    DateType::class,
+            //    array_merge(
+            //        array(
+            //            'property_path' => 'start',
+            //            'widget' => 'single_text',
+            //            'format' => 'dd/MM/yyyy',
+            //        )
+            //    ))
             ->add('frequency_period', ChoiceType::class, ['choices' => ['month', 'week']])
             ->add('surface_m2', NumberType::class)
             ->add('surface_type', ChoiceType::class, ['choices' => ['wood', 'concrete']])
