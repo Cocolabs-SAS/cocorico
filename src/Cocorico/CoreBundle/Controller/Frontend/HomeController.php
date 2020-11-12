d23ad03692e67211c833d8d9db8c4f6b34edb7f1
pmontens <pieterjan@montens.net>
2020-11-12 14:23:08 +0100

    :wrench: css update, font testing

--- src/Cocorico/CoreBundle/Controller/Frontend/HomeController.php
+++ src/Cocorico/CoreBundle/Controller/Frontend/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
     {
         $listings = $this->get("cocorico.listing_search.manager")->getHighestRanked(
             $this->get('cocorico.listing_search_request'),
-            18,
+            42,
             $request->getLocale(),
             $this->getParameter('cocorico.listing_highestrank_cache_age')
         );
