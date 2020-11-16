8c21768a9c6fe678c1b09ad9a5b2cb6d9d586d44
pmontens <pieterjan@montens.net>
2020-11-16 13:30:35 +0100

    :seedling: fixing proxy rules

--- web/app.php
+++ web/app.php
@@ -13,6 +13,21 @@ $kernel = new AppCache($kernel);
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
+
+Request::setTrustedProxies(
+    // the IP address (or range) of your proxy
+    ['10.0.0.0/8'],
+
+    // trust *all* "X-Forwarded-*" headers
+    Request::HEADER_X_FORWARDED_ALL
+
+    // or, if your proxy instead uses the "Forwarded" header
+    // Request::HEADER_FORWARDED
+
+    // or, if you're using AWS ELB
+    // Request::HEADER_X_FORWARDED_AWS_ELB
+);
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
