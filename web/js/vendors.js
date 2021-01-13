c8591424a181324ac72e60860f2b62ee997e58ff
pmontens <pieterjan@montens.net>
2021-01-13 09:54:09 +0100

    :clipboard: adding missing file

---
+++ web/js/vendors.js
@@ -0,0 +1,13 @@
+// const jQuery = require('jquery');
+require('jquery-ui');
+require('jquery-ui-touch-punch');
+
+const jcf = require('jcf');
+
+require('bootstrap');
+
+const Cookies = require('js-cookie');
+
+window.$ = window.jQuery = jQuery;
+window.Cookies = Cookies;
+window.jcf = jcf;
