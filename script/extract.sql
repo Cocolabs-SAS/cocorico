12723599beddef8104b1ae12d2b8ef5e27f63191
pmontens <pieterjan@montens.net>
2021-02-11 12:19:46 +0100

    :wrench: adding script extract function

---
+++ script/extract.sql
@@ -0,0 +1,27 @@
+SELECT u.id,
+       u.last_name,
+       u.first_name,
+       u.phone,
+       u.email,
+       siae.siret,
+       siae.naf,
+       siae.kind,
+       siae.name,
+       siae.brand,
+       siae.phone,
+       siae.email,
+       siae.website,
+       siae.description,
+       siae.address_line_1,
+       siae.address_line_2,
+       siae.post_code,
+       siae.city,
+       siae.department,
+       siae.source,
+       ST_X(siae.coords::geometry) AS longitude,
+       ST_Y(siae.coords::geometry) AS latitude,
+       convention.is_active
+FROM siaes_siae AS siae
+LEFT OUTER JOIN siaes_siaeconvention AS convention ON convention.id = siae.convention_id
+LEFT OUTER JOIN siaes_siaemembership AS membership ON membership.siae_id = siae.id
+LEFT OUTER JOIN users_user AS u ON membership.user_id = u.id
