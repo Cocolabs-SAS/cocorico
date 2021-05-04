8099050d33c04933704806db72f245c6fb0ef242
pmontens <pieterjan@montens.net>
2021-05-04 18:12:04 +0200

    :whale: local docker improvements

--- docker/docker_full_init.sh
+++ docker/docker_full_init.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
-
-## INIT MONGO
-mongod --bind_ip 0.0.0.0 --fork --logpath /var/log/mongod.log
-
-
 ## INIT MYSQL
 if [[ ! -d /run/mysqld ]]; then
     mkdir -p /run/mysqld
 fi
 
+echo "Installing Mysql"
 if [[ ! -d /var/lib/mysql/cocorico ]]; then
-    mysql_install_db --user=root --datadir=/var/lib/mysql
-    mysqld --user=root --bootstrap < ./docker/database.sql
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    mysqld --user=mysql --bootstrap < ./docker/database.sql
 fi
 
 chmod 777 -R /var/lib/mysql
-mysqld_safe --user=root
-while !(mysqladmin -ucocorico -pcocorico pin-v `pwd`:/cocorico -v `pwd`/tmp/mysql:/var/lib/mysql -v `pwd`/tmp/mongo:/data/dbg &> /dev/null); do
+chown mysql:mysql -R /var/run/mysqld
+echo "Running Mysql"
+mysqld_safe --no-watch
+while !(mysqladmin -ucocorico -pcocorico pin &> /dev/null); do
     sleep 1
     echo "Waiting for MySQL to come up"
 done
 
 if [[ ! -f /var/lib/mysql/cocorico/db.opt ]]; then
+    echo "Filling Mysql"
     php bin/console doctrine:schema:update --force
     php bin/console doctrine:fixtures:load -n
-fi
-
-if [[ ! -d /data/db ]]; then
-    php bin/console doctrine:mongodb:schema:create
 fi
---
+++ docker/run_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+chmod -R 777 /cocorico/var
+export ITOU_ENV=local_dev
+clevercloud/post_build_hook.sh
+/home/cocorico/.symfony/bin/symfony server:start --no-tls --port 80 &
+yarn encore dev --watch
